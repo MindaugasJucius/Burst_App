ba91eb05cf7c407d8d6ec8523761896c41d6f61c
Mindaugas Jucius <mindaugas.jucius@vinted.com>
2016-09-13 20:22:03 +0300

    Introduce BurstAPI framework

---
+++ BurstAPI/BurstAPI/BurstAPI.h
@@ -0,0 +1,19 @@
+//
+//  BurstAPI.h
+//  BurstAPI
+//
+//  Created by Mindaugas Jucius on 13/09/16.
+//  Copyright © 2016 mindaugo.kompanija.limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BurstAPI.
+FOUNDATION_EXPORT double BurstAPIVersionNumber;
+
+//! Project version string for BurstAPI.
+FOUNDATION_EXPORT const unsigned char BurstAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BurstAPI/PublicHeader.h>
+
+
---
+++ BurstAPI/BurstAPI/Entities/Entity.swift
@@ -0,0 +1,5 @@
+public class Entity: NSObject {
+    public class func printFunky() {
+        print("funki boi")
+    }
+}
