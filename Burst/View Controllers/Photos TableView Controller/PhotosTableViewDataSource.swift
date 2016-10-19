76c6d1bbed394af2ec05b700d317ad90ea81247c
Mindaugas Jucius <mindaugas.jucius@vinted.com>
2016-10-19 18:20:21 +0300

    Add prototype cell

--- Burst/View Controllers/Photos TableView Controller/PhotosTableViewDataSource.swift
+++ Burst/View Controllers/Photos TableView Controller/PhotosTableViewDataSource.swift
@@ -86,10 +86,10 @@ class PhotosTableViewDataSource: NSObject {
     }
     
     private func registerViews() {
-        let cellNib = UINib.init(nibName: PhotoTableViewCell.className(), bundle: nil)
+        //let cellNib = UINib.init(nibName: PhotoTableViewCell.className(), bundle: nil)
         let headerNib = UINib.init(nibName: PhotoHeader.className(), bundle: nil)
         
-        tableView.register(cellNib, forCellReuseIdentifier: PhotoTableViewCell.reuseIdentifier)
+        //tableView.register(cellNib, forCellReuseIdentifier: PhotoTableViewCell.reuseIdentifier)
         tableView.register(headerNib, forHeaderFooterViewReuseIdentifier: PhotoHeader.reuseIdentifier)
     }
     
