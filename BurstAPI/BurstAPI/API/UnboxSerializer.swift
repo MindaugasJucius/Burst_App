d277833304c36c79c2583f426cfce28ba9970070
Mindaugas Jucius <mindaugas.jucius@vinted.com>
2016-10-19 16:20:24 +0300

    Implement serializer

--- BurstAPI/BurstAPI/API/UnboxSerializer.swift
+++ BurstAPI/BurstAPI/API/UnboxSerializer.swift
@@ -6,11 +6,16 @@ public typealias EntityCallback<U: Unboxable> = (_ entity: U) -> ()
 public typealias ErrorCallback = (NSError) -> ()
 
 class UnboxSerializer: NSObject {
-
-    static func parse<U: Unboxable>(_ response: UnboxableDictionary, success: @escaping EntityCallback<U>, failure: @escaping ErrorCallback) {
+    
+    static func parse<U: Unboxable>(response: NSDictionary, success: @escaping EntityCallback<U>, failure: @escaping ErrorCallback) {
         DispatchQueue.global(qos: .userInitiated).async {
             do {
-                let parsedEntity: U = try unbox(dictionary: response)
+                guard let unboxDictionary = response as? UnboxableDictionary else {
+                    let error = NSError(domain: "Unable to parse model: \(String(describing: U.self))", code: 0, userInfo: nil)
+                    failure(error)
+                    return
+                }
+                let parsedEntity: U = try unbox(dictionary: unboxDictionary)
                 DispatchQueue.main.async {
                     success(parsedEntity)
                 }
@@ -22,6 +27,32 @@ class UnboxSerializer: NSObject {
         }
     }
     
-    
+    static func parse<U: Unboxable>(responses: [NSDictionary], success: @escaping EntitiesCallback<U>, failure: @escaping ErrorCallback) {
+        let parseGroup = DispatchGroup()
+        var parsedArray: [U] = []
+        
+        responses.forEach { response in
+            parseGroup.enter()
+            UnboxSerializer.parse(response: response,
+                success: { (serialized: U) in
+                    parsedArray.append(serialized)
+                    parseGroup.leave()
+                },
+                failure: { error in
+                    parseGroup.leave()
+                    parseGroup.notify(queue: DispatchQueue.main,
+                        execute: {
+                            failure(error)
+                        }
+                    )
+                }
+            )
+        }
+        parseGroup.notify(queue: DispatchQueue.main,
+            execute: {
+                success(parsedArray)
+            }
+        )
+    }
     
 }
