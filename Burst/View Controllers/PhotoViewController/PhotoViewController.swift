5d3ddfe671049838eb4b091a8321f2818d352f85
Mindaugas Jucius <mindaugas.jucius@vinted.com>
2016-11-17 14:02:39 +0200

    Add necessary classes

---
+++ Burst/View Controllers/PhotoViewController/PhotoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PhotoViewController.swift
+//  Burst
+//
+//  Created by Mindaugas Jucius on 17/11/2016.
+//  Copyright © 2016 mindaugo.kompanija.limited. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
