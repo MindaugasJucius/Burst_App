a24f27e8b30d3da70b0373a4b356b5f84715bf0b
Mindaugas Jucius <mindaugas.jucius@vinted.com>
2016-11-17 14:02:39 +0200

    Add necessary classes

---
+++ Burst/View Controllers/PhotoViewController/PhotoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoTableViewCell.swift
+//  Burst
+//
+//  Created by Mindaugas Jucius on 17/11/2016.
+//  Copyright © 2016 mindaugo.kompanija.limited. All rights reserved.
+//
+
+import UIKit
+
+class PhotoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
