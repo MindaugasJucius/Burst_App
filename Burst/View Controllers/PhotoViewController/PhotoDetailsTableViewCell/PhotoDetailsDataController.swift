0355bcad0e702efe12cd9cd8d78a82180969c537
Mindaugas Jucius <mindaugas.jucius@vinted.com>
2016-11-18 11:36:33 +0200

    Move PhotoViewController dependencies to folders

---
+++ Burst/View Controllers/PhotoViewController/PhotoDetailsTableViewCell/PhotoDetailsDataController.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoDetailsDataController.swift
+//  Burst
+//
+//  Created by Mindaugas Jucius on 18/11/2016.
+//  Copyright © 2016 mindaugo.kompanija.limited. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailsDataController: NSObject {
+
+}
