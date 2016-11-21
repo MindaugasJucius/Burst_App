53518558422f023d6fcbbd81bd1e2a7c9fdee30c
Mindaugas Jucius <mindaugas.jucius@vinted.com>
2016-11-21 14:43:16 +0200

    Add InsetButton subclass

---
+++ Burst/Common Views/InsetButton.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+class InsetButton: UIButton {
+
+    override var contentEdgeInsets: UIEdgeInsets {
+        get {
+            return UIEdgeInsets(top: 1, left: 15, bottom: 0, right: 15)
+        }
+        set {
+            self.contentEdgeInsets = newValue
+        }
+    }
+    
+    override var isHighlighted: Bool {
+        didSet {
+            if isHighlighted {
+                backgroundColor = .white
+            } else {
+                backgroundColor = nil
+            }
+        }
+    }
+    
+    var onButtonTap: (() -> ())?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.cornerRadius = 3
+        layer.borderWidth = 0.5
+        layer.borderColor = UIColor.white.cgColor
+        setTitleColor(.white, for: .normal)
+        setTitleColor(.black, for: .highlighted)
+        
+        titleLabel?.font = AppAppearance.regularFont(withSize: .sectionHeaderTitle)
+        addTarget(self, action: #selector(tapped(button:)), for: .touchUpInside)
+    }
+    
+    override func setTitle(_ title: String?, for state: UIControlState) {
+        super.setTitle(title?.uppercased(), for: state)
+    }
+
+    
+    @objc func tapped(button: UIButton) {
+        onButtonTap?()
+    }
+    
+}
--- Burst/Common Views/TableViewHeaderWithButton.swift
+++ Burst/Common Views/TableViewHeaderWithButton.swift
@@ -2,28 +2,43 @@ import UIKit
 
 class TableViewHeaderWithButton: UITableViewHeaderFooterView, ReusableView {
 
-    @IBOutlet private weak var button: UIButton!
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet private weak var button: InsetButton!
     @IBOutlet private weak var label: UILabel!
-    private var onButtonTap: (() -> ())?
     
     override func awakeFromNib() {
+        super.awakeFromNib()
+        initialSetup()
+    }
+    
+    // MARK: - Public
+    
+    func configure(labelTitle: String, hideButton: Bool = true, hideImage: Bool = true, buttonTitle: String? = nil, onButtonTap: (() -> ())? = nil) {
+        button.setTitle(buttonTitle, for: .normal)
+        button.isHidden = hideButton
+        button.onButtonTap = onButtonTap
+        label.text = labelTitle.capitalized
+        guard hideImage else {
+            return
+        }
+        setupConstraints()
+    }
+    
+    // MARK: - Private
+    
+    private func initialSetup() {
+        contentView.backgroundColor = AppAppearance.tableViewBackground
         label.textColor = AppAppearance.subtitleColor
         label.font = AppAppearance.condensedFont(withSize: .headerTitle)
-        button.setTitleColor(.white, for: .normal)
-        button.titleLabel?.font = AppAppearance.regularFont(withSize: .headerSubtitle)
-        contentView.backgroundColor = AppAppearance.tableViewBackground
-        button.addTarget(self, action: #selector(tapped(button:)), for: .touchUpInside)
+        label.lineBreakMode = .byWordWrapping
     }
     
-    func configure(labelTitle: String, hideButton: Bool = true, buttonTitle: String? = nil, onButtonTap: (() -> ())? = nil) {
-        button.setTitle(buttonTitle?.uppercased(), for: .normal)
-        button.isHidden = hideButton
-        self.onButtonTap = onButtonTap
-        label.text = labelTitle.capitalized
-    }
-    
-    @objc func tapped(button: UIButton) {
-        onButtonTap?()
+    private func setupConstraints() {
+        leftImageView.removeFromSuperview()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.leadingAnchor.constraint(
+            equalTo: contentView.leadingAnchor,
+            constant: 15).isActive = true
     }
     
 }
