9f32257a32778e26669ea99b7286babb5bc7e9de
Mindaugas Jucius <mindaugas.jucius@vinted.com>
2016-11-21 11:11:07 +0200

    Add author screen files

---
+++ Burst/View Controllers/AuthorViewController/AuthorViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class AuthorViewController: UIViewController {
+
+    @IBOutlet private weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+    
+}
