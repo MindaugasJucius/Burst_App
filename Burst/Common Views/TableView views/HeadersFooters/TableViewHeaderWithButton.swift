d93c79fd3fe8b8788851f58e8c950ff25c7dcf9d
Mindaugas Jucius <mindaugas.jucius@vinted.com>
2016-11-25 14:16:38 +0200

    Update header constraints

--- Burst/Common Views/TableView views/HeadersFooters/TableViewHeaderWithButton.swift
+++ Burst/Common Views/TableView views/HeadersFooters/TableViewHeaderWithButton.swift
@@ -3,9 +3,12 @@ import AlamofireImage
 
 class TableViewHeaderWithButton: UITableViewHeaderFooterView, ReusableView {
 
-    @IBOutlet weak var leftImageView: UIImageView?
+    @IBOutlet weak var leftImageView: UIImageView!
     @IBOutlet private weak var button: InsetButton!
     @IBOutlet private weak var label: UILabel!
+    
+    var labelToImageView: NSLayoutConstraint?
+    var labelToContentView: NSLayoutConstraint?
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -14,15 +17,12 @@ class TableViewHeaderWithButton: UITableViewHeaderFooterView, ReusableView {
     
     // MARK: - Public
     
-    func configure(labelTitle: String, hideButton: Bool = true, hideImage: Bool = true, buttonTitle: String? = nil, onButtonTap: (() -> ())? = nil) {
+    func configure(labelTitle: String, showButton: Bool = false, showImage: Bool = false, buttonTitle: String? = nil, onButtonTap: (() -> ())? = nil) {
         label.text = labelTitle
         button.setTitle(buttonTitle, for: .normal)
-        button.isHidden = hideButton
+        button.isHidden = !showButton
         button.onButtonTap = onButtonTap
-        guard hideImage else {
-            return
-        }
-        hideImageView()
+        updateImageView(visible: showImage)
     }
     
     func image(withURL url: URL) {
@@ -40,16 +40,25 @@ class TableViewHeaderWithButton: UITableViewHeaderFooterView, ReusableView {
         label.font = AppAppearance.regularFont(withSize: .headerTitle, weight: .medium)
         contentView.backgroundColor = AppAppearance.tableViewBackground
         label.lineBreakMode = .byWordWrapping
+        labelToImageView = label.leadingAnchor.constraint(
+            equalTo: leftImageView.trailingAnchor,
+            constant: 10)
+        labelToContentView = label.leadingAnchor.constraint(
+            equalTo: contentView.leadingAnchor,
+            constant: 15)
     }
     
-    private func hideImageView() {
-        guard leftImageView != nil else {
-            return
-        }
-        leftImageView?.removeFromSuperview()
-        label.leadingAnchor.constraint(
-            equalTo: contentView.leadingAnchor,
-            constant: 15).isActive = true
+    private func updateImageView(visible: Bool) {
+        leftImageView.isHidden = !visible
+        labelToImageView?.isActive = visible
+        labelToContentView?.isActive = !visible
+    }
+    
+    override func prepareForReuse() {
+        leftImageView.image = nil
+        labelToImageView?.isActive = false
+        labelToContentView?.isActive = false
+        leftImageView.isHidden = false
     }
     
 }
