cea1be8f548202d6cbdaacb77b7aae482ba542b0
Mindaugas Jucius <mindaugas.jucius@vinted.com>
2016-12-30 13:55:50 +0200

    add query parameter to UnsplashGeneric network calls

--- BurstAPI/BurstAPI/API/Calls/UnsplashGeneric.swift
+++ BurstAPI/BurstAPI/API/Calls/UnsplashGeneric.swift
@@ -3,15 +3,15 @@ import Unbox
 
 public typealias SingleEntityCallback<U: Unboxable> = (_ unboxableEntity: U) -> ()
 public typealias EntityArrayCallback<U: Unboxable> = (_ unboxableArray: [U]) -> ()
+public typealias ParamsDict = [String: Any]
 
 public class UnsplashGeneric: NSObject {
 
     public static func unsplash<U: Unboxable>(getFromURL url: URL,
-                             success: @escaping SingleEntityCallback<U>,
-                             failure: ErrorCallback?) {
-        guard let appID = AppConstants.appConstDict[BurstID] else { return }
-        let params = [BurstID : appID] as [String : Any]
-        Alamofire.request(url, method: .get, parameters: params).validate().responseJSON { response in
+                                queryParams: ParamsDict? = nil,
+                                success: @escaping SingleEntityCallback<U>,
+                                failure: ErrorCallback?) {
+        Alamofire.request(url, method: .get, parameters: paramsDict(additionalValues: queryParams)).validate().responseJSON { response in
             switch response.result {
             case .success(let value):
                 guard let responseJSON = value as? NSDictionary else { return }
@@ -30,11 +30,10 @@ public class UnsplashGeneric: NSObject {
     }
     
     public static func unsplashArray<U: Unboxable>(getFromURL url: URL,
-                                success: @escaping EntityArrayCallback<U>,
-                                failure: ErrorCallback?) {
-        guard let appID = AppConstants.appConstDict[BurstID] else { return }
-        let params = [BurstID : appID] as [String : Any]
-        Alamofire.request(url, method: .get, parameters: params).responseJSON { response in
+                                     queryParams: [String: Any]? = nil,
+                                     success: @escaping EntityArrayCallback<U>,
+                                     failure: ErrorCallback?) {
+        Alamofire.request(url, method: .get, parameters: paramsDict(additionalValues: queryParams)).responseJSON { response in
             switch response.result {
             case .success(let value):
                 guard let responseJSON = value as? [NSDictionary] else { return }
@@ -50,5 +49,17 @@ public class UnsplashGeneric: NSObject {
                 failure?(error)
             }
         }
+    }
+    
+    private static func paramsDict(additionalValues: ParamsDict?) -> ParamsDict {
+        var params = [BurstID : AppConstants.appConstDict[BurstID]!] as ParamsDict
+        guard let additionalDict = additionalValues else {
+            return params
+        }
+        for key in additionalDict.keys {
+            guard let value = additionalDict[key] else { continue }
+            params.updateValue(value, forKey: key)
+        }
+        return params
     }
 }
